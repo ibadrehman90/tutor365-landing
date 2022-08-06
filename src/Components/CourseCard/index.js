59c5ac865b9cd73999888fd9a63d7a6569eb2755
EzanAsif <ezanasif1972@gmail.com>
2022-08-06 10:19:41 +0500

    Added course cards

--- src/Components/CourseCard/index.js
+++ src/Components/CourseCard/index.js
@@ -1,29 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { BiCheck } from "react-icons/bi";
 import "./styles.css";
 
-const CourseCard = () => {
+const CourseCard = ({
+  courseLevel = "Beginner",
+  courseCardImageUrl = "https://wordpress-774869-2749119.cloudwaysapps.com/wp-content/uploads/2022/05/colleagues-learning-together-during-group-study-scaled.jpeg",
+  courseTitle = "From Scratch",
+  courseDescription = "Course Description",
+  coursePrice = "$31.99",
+  courseBenefits = [
+    { benefit: "Benefit", isAvailable: false },
+    { benefit: "Benefit", isAvailable: false },
+    { benefit: "Benefit", isAvailable: true },
+  ],
+}) => {
   return (
     <div className="CourseCard">
-      <div className="CourseCardLevel">Beginner</div>
+      <div className="CourseCardLevel">{courseLevel}</div>
       <div className="CourseCardIndividualDetail">
         <img
           style={{ width: "100%", alignSelf: "center" }}
           height={200}
-          src="https://wordpress-774869-2749119.cloudwaysapps.com/wp-content/uploads/2022/05/colleagues-learning-together-during-group-study-scaled.jpeg"
-          alt=""
+          src={courseCardImageUrl}
+          alt={courseTitle}
         />
       </div>
       <div className="CourseCardDetails">
         <div className="CourseCardIndividualDetail">
-          <h2>GCSE</h2>
+          <h2>{courseTitle}</h2>
         </div>
         <div className="CourseCardIndividualDetail">
-          <p>Covers Maths, English, and Science for primary students.</p>
+          <p>{courseDescription}</p>
         </div>
         <div className="CourseCardIndividualDetail">
-          <h2 className="price">$31.99/mo</h2>
+          <h2 className="price">{coursePrice}/month</h2>
         </div>
         <div className="CourseCardIndividualDetail">
           <button className="subscribeBtn" to={"/"}>
@@ -32,12 +42,19 @@ const CourseCard = () => {
         </div>
         <div className="CourseCardIndividualDetail">
           <ul>
-            <li>
-              <span>
-                <BiCheck color="green" fontSize={24} />
-              </span>
-              Live Classes
-            </li>
+            {courseBenefits.map((obj, index) => {
+              return (
+                <li key={index} className={!obj.isAvailable && "disabled"}>
+                  <span>
+                    <BiCheck
+                      fontSize={24}
+                      style={{ marginTop: 2, color: "#00de00" }}
+                    />
+                  </span>
+                  {obj.benefit}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
--- src/Components/Sections/AvailableCourses/index.js
+++ src/Components/Sections/AvailableCourses/index.js
@@ -2,10 +2,65 @@ import React from "react";
 import CourseCard from "../../CourseCard";
 import "./styles.css";
 const AvailableCourses = () => {
+  let data = [
+    {
+      level: "Beginner",
+      imageUrl:
+        "https://wordpress-774869-2749119.cloudwaysapps.com/wp-content/uploads/2022/05/virtual-classroom-study-space-scaled.jpeg",
+      title: "From Scratch",
+      description: "Covers Maths, English, and Science for primary students.",
+      price: "£9.50",
+      benefits: [
+        { benefit: "Recorded Lecture", isAvailable: true },
+        { benefit: "Assignments", isAvailable: true },
+        { benefit: "Live Class", isAvailable: false },
+      ],
+    },
+    {
+      level: "Intermediate",
+      imageUrl:
+        "https://wordpress-774869-2749119.cloudwaysapps.com/wp-content/uploads/2022/05/colleagues-learning-together-during-group-study-scaled.jpeg",
+      title: "GCSE",
+      description: "Covers Maths, English, and Science for GCSE students.",
+      price: "£9.50",
+      benefits: [
+        { benefit: "Recorded Lecture", isAvailable: true },
+        { benefit: "Assignments", isAvailable: true },
+        { benefit: "Live Class", isAvailable: true },
+      ],
+    },
+    {
+      level: "Advanced",
+      imageUrl:
+        "https://wordpress-774869-2749119.cloudwaysapps.com/wp-content/uploads/2022/05/student-class-looking-course-scaled.jpeg",
+      title: "GCSE",
+      description: "Covers Maths, English, and Science for GCSE students.",
+      price: "£9.50",
+      benefits: [
+        { benefit: "Recorded Lecture", isAvailable: true },
+        { benefit: "Assignments", isAvailable: true },
+        { benefit: "Live Class", isAvailable: true },
+      ],
+    },
+  ];
   return (
-    <div>
+    <div className="AvailableCourses">
       <h2>Available Courses</h2>
-      <CourseCard />
+      <div className="AvailableCoursesList">
+        {data.map((obj, index) => {
+          return (
+            <CourseCard
+              key={index}
+              courseLevel={obj.level}
+              courseCardImageUrl={obj.imageUrl}
+              courseTitle={obj.title}
+              courseDescription={obj.description}
+              coursePrice={obj.price}
+              courseBenefits={obj.benefits}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
