3984c1d9d4605da8b3aa2e2743a024d6e474628d
Ezan Asif <53692707+EzanAsif@users.noreply.github.com>
2022-10-20 11:46:48 +0500

    Updated backend api and domain link

--- src/App/api.js
+++ src/App/api.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://192.168.18.63:8000/api/v1";
-export const BASE_URL = "https://tutor365-backend.herokuapp.com";
-export const STUDENT_PORTAL_PAGE_URL = "http://portal.tutor365.com:3001/";
+export const BASE_URL = "https://93npni54x6.execute-api.eu-west-2.amazonaws.com/prod";
+export const STUDENT_PORTAL_PAGE_URL = "http://portal.tutor365.co.uk";
